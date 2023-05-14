40aec7ca98c2c8a0bcbf30fec769dd1b856244a8
Akash Suresh <akashsuresh@Akashs-MacBook-Pro.local>
2023-05-14 14:57:59 +0100

    Added SoftMax

---
+++ algorithms/BanditAlogrithm.py
@@ -0,0 +1,9 @@
+class BanditAlgorithm:
+    def initialize(self, n_arms):
+        pass
+
+    def select_arm(self):
+        pass
+
+    def update(self, chosen_arm, reward):
+        pass
--- algorithms/SoftMax.py
+++ algorithms/SoftMax.py
@@ -1,13 +1,21 @@
-import random
 from algorithms.BanditAlogrithm import BanditAlgorithm
+import random
+import math
 
-def ind_max(x):
-    m = max(x)
-    return x.index(m)
+def categorical_draw(probs):
+    z = random.random()
+    cum_prob = 0.0
+    for i in range(len(probs)):
+        prob = probs[i]
+        cum_prob += prob
+        if cum_prob > z:
+            return i
+    return len(probs) - 1
 
-class EpsilonGreedy(BanditAlgorithm):
-    def __init__(self, epsilon, counts, values):
-        self.epsilon = epsilon
+class SoftMax(BanditAlgorithm):
+    
+    def __init__(self, temperature, counts, values):
+        self.temperature = temperature
         self.counts = counts
         self.values = values
         return
@@ -17,23 +25,15 @@ class EpsilonGreedy(BanditAlgorithm):
         self.values = [0.0 for col in range(n_arms)]
         return
     
-    
     def select_arm(self):
-        if random.random() > self.epsilon:
-            # exploitation
-            return ind_max(self.values)
-        else:
-            # exploration
-            return random.randrange(len(self.values))
-        
+        z = sum([math.exp(v / self.temperature) for v in self.values])
+        probs = [math.exp(v / self.temperature) / z for v in self.values]
+        return categorical_draw(probs)
+    
     def update(self, chosen_arm, reward):
         self.counts[chosen_arm] = self.counts[chosen_arm] + 1
         n = self.counts[chosen_arm]
         value = self.values[chosen_arm]
         new_value = ((n - 1) / float(n)) * value + (1 / float(n)) * reward
         self.values[chosen_arm] = new_value
-        # print('counts = ' + str(self.counts))
-        # print('values = ' + str(self.values))
         return
-    
-
